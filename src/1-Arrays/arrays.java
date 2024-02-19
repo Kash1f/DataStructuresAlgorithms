45ad1afa398eb87370a3fc4c56332344bc52d174
Kashif <ahmed.kashif1001@gmail.com>
2024-02-20 00:34:12 +0500

    arrays example 1

--- src/1-Arrays/arrays.java
+++ src/1-Arrays/arrays.java
@@ -1,5 +1,19 @@
 public class arrays{
     public static void main(String[] args) {
-        System.out.println("Arrays Start");
+
+
+        //datatype[] variable_name = new int[];
+        //store 5 roll numbers in an array
+
+        int[] roll_no = new int[5];
+
+        //directly we can write it as:
+
+        int[] roll_no2 = {10,20,22,35,40}; //Here the int data type represents the type of data stored in array
+
+        //arrays can only have similar data type
+        //int means only integers in the array, float means only float values in the array
+
+       
     }
     }
