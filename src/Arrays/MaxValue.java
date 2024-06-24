7ec0f63e2028e4121196d28a16e50c749dadc85c
Kashif <ahmed.kashif1001@gmail.com>
2024-06-24 23:17:42 +0500

    Q3 reversing an array

--- src/Arrays/MaxValue.java
+++ src/Arrays/MaxValue.java
@@ -1,7 +1,20 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MaxValue {
     public static void main(String[] args) {
-        
+        int[] arr = {1, 4, 24, 10, 30};
+        System.out.println(max(arr));
     }
+    static int max(int[] arr){
+        int maxVal = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if (arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+   
 }
