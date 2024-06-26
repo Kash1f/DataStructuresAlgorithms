4275ceb96c5aa8831c3e2a9a685cad034e366c74
Kashif <ahmed.kashif1001@gmail.com>
2024-06-26 23:52:29 +0500

    calculator program java

---
+++ src/ConditionalsLoops/CalculatorProgram.java
@@ -0,0 +1,16 @@
+package ConditionalsLoops;
+
+import java.util.Scanner;
+
+public class CalculatorProgram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Take input from user till user does not press X or x
+
+        while(true) {
+            //take the operator as input
+
+            char op = in.next().trim().charAt(0);
+        }
+    }
+}
