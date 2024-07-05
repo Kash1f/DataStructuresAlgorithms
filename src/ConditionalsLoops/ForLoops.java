19ba5440442b873f74d451f158e62d1a0b37c570
Kashif <ahmed.kashif1001@gmail.com>
2024-07-05 23:52:27 +0500

    conditonal and loops

---
+++ src/ConditionalsLoops/ForLoops.java
@@ -0,0 +1,19 @@
+package ConditionalsLoops;
+
+public class ForLoops{
+    public static void main(String[] args) {
+
+        /*
+         Syntax of For Loop
+
+         for(initialization; condition; increment/decrement)
+
+         */
+
+        //Print numbers from 1-5
+
+        for(int num = 1; num <= 5; num++){
+            System.out.println(num);
+        }
+    }
+}
