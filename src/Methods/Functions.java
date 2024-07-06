12cbb4293c765dd28697efc0c9bf63acc2a7d290
Kashif <ahmed.kashif1001@gmail.com>
2024-07-06 23:57:42 +0500

    function basics

---
+++ src/Methods/Functions.java
@@ -0,0 +1,18 @@
+package Methods;
+
+class Car{
+    static void myFunc(){
+        System.out.println("Hello World");
+    }
+}
+
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        Car.myFunc();
+    }
+    
+}
