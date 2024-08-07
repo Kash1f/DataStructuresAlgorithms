53257782661ac1b3a66d3b1a96c166293a3257e3
Kashif <ahmed.kashif1001@gmail.com>
2024-08-07 23:51:15 +0500

    graph example

---
+++ src/Graphs/Example.java
@@ -0,0 +1,17 @@
+package Graphs;
+
+class Car{
+    static void myFunc(){
+        System.out.println("Hello World");
+    }
+}
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+        Car.myFunc();
+    }
+    
+}
