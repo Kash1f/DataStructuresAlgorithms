566c3a83a20263043448a6ad2e36db8863ea15cc
Kashif <ahmed.kashif1001@gmail.com>
2024-08-09 23:56:46 +0500

    stacks

--- src/Stacks/Stacks.java
+++ src/Stacks/Stacks.java
@@ -1,17 +1,21 @@
 package Stacks;
+
 import java.util.Stack;
 
 public class Stacks {
     public static void main(String[] args) {
-        
-    //We first need to declare our stack and instantiate it
-    //it will have the data type of the objects that we will be adding to our stack
 
-    //Here we will create a Stack that store names of video games
+        // We first need to declare our stack and instantiate it
+        // it will have the data type of the objects that we will be adding to our stack
 
-    Stack <String> stack = new Stack<String>();
+        // Here we will create a Stack that store names of video games
 
-    System.out.println(stack.empty());
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("CS2");
+        stack.push("Call of duty");
+        stack.push("Battlefield");
+        System.out.println(stack);
 
     }
 }
