8c94c388316784b46c33c01eb2d4385c6baffc91
Kashif <ahmed.kashif1001@gmail.com>
2024-08-12 21:41:23 +0500

    queue, java collections framework

---
+++ src/Queues/QueueExample.java
@@ -0,0 +1,19 @@
+//Queues Explanation:
+
+//Queue = FIFO Data Structure. First-In First-Out
+//        A data structure that holds elements prior to processing
+ //       (eg: A line at a cash counter)
+ //       It is a Linear Data Structure
+
+//         add = enqueue, offer()
+//         remove = dequeue, poll()
+
+ //The customer that reaches the counter first is entertained first and rest after him accordingly i.e first come first serve, the firt one is at the Head of the Queue while the last one is at the Tail of the Queue. The second one will be at the Head once the first one is out of the Queue.
+
+package Queues;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        
+    }
+}
