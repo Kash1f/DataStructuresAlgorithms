47144bf5791032a299dc8d8915930d42d227cfad
Kashif <ahmed.kashif1001@gmail.com>
2024-08-16 22:16:24 +0500

    algorith analysis method2

--- src/AlgorithmAnalysis/Method2.java
+++ src/AlgorithmAnalysis/Method2.java
@@ -1,5 +1,24 @@
 package AlgorithmAnalysis;
 
+import java.util.Scanner;
+
 public class Method2 {
-    
+
+    // Method to find the sum of first n natural numbers
+    public int findSum(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        sc.close(); // Close the scanner to avoid resource leaks
+
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        Method2 obj = new Method2(); // Create an instance of the Method2 class
+        int result = obj.findSum();  // Call the findSum method using the instance
+        System.out.println("The sum is: " + result);  // Print the result
+    }
 }
