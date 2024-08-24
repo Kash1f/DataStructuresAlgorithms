c752ab5216f8571a0767688ada4c4f0d2aa8287b
Kashif <ahmed.kashif1001@gmail.com>
2024-08-24 23:44:25 +0500

    bubble sort

---
+++ src/Sorting/BubbleSort/Example.java
@@ -0,0 +1,18 @@
+package Sorting.BubbleSort;
+
+public class Example{
+    public static void main(String[] args) {
+
+
+            int nums[] = {6,5,2,8,9,4};
+        System.out.println("Before Sorting");
+
+        for(int num:nums){
+            System.out.println(num + "");
+        }
+
+
+
+    }
+}
+
