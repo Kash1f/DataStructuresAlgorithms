4b792e5e5c0797dc3f3ea2bad8ef4f314bd0cd78
Kashif <ahmed.kashif1001@gmail.com>
2024-08-24 23:47:12 +0500

    examples

--- src/Sorting/BubbleSort/Example.java
+++ src/Sorting/BubbleSort/Example.java
@@ -1,18 +1,23 @@
 package Sorting.BubbleSort;
 
-public class Example{
+public class Example {
     public static void main(String[] args) {
 
+        int nums[] = { 6, 5, 2, 8, 9, 4 };
+        int size = nums.length;
+        int temp = 0;
 
-            int nums[] = {6,5,2,8,9,4};
         System.out.println("Before Sorting");
-
-        for(int num:nums){
+        for (int num : nums) {
             System.out.println(num + "");
         }
 
-
+        for(int i=0; i<size;i++){
+            for(int j=0;j<size;j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                }
+            }
 
     }
 }
-
