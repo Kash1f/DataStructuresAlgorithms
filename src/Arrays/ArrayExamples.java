133f0dcf31c1b759a11365ef27f89e0414645868
Kashif <ahmed.kashif1001@gmail.com>
2024-08-25 22:52:27 +0500

    arrays.toString, arrays.fill

--- src/Arrays/ArrayExamples.java
+++ src/Arrays/ArrayExamples.java
@@ -10,7 +10,11 @@ public class ArrayExamples {
 
 
         int[] arr2 = new int[10];
+
+        //The fill method is gonna fill this array with 12 at each position(index)
         Arrays.fill(arr2, 12);
+
+        //Here we have updated the value of 0th index
         arr2[0] = 10;
         System.out.println(Arrays.toString(arr2));
     }
