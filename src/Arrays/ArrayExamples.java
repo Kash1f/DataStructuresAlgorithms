d3e0307e941fdfeac1b7cdb89cf02101a0827f7d
Kashif <ahmed.kashif1001@gmail.com>
2024-08-26 14:07:25 +0500

    enhanced forloop/foreach loop

--- src/Arrays/ArrayExamples.java
+++ src/Arrays/ArrayExamples.java
@@ -4,20 +4,31 @@ import java.util.Arrays;
 
 public class ArrayExamples {
     public static void main(String[] args) {
-        int[] arr = { 1, 14, 6, 8, 19 };
+        int[] array = { 1, 14, 6, 8, 19 };
 
-        Arrays.sort(arr); // O(nlogn)
-        System.out.println(Arrays.toString(arr));
+        Arrays.sort(array); // O(nlogn)
+        System.out.println(Arrays.toString(array)); //we will have sorted array here
 
         // Traversal
-        System.out.println("Array Length" + arr.length); // this will tell that this array has 5 elements
+        System.out.println("Array Length " + array.length); // this will tell that this array has 5 elements
 
         // When i = 0, arr[i] is arr[0], which gives you 1.
         // When i = 1, arr[i] is arr[1], which gives you 14.
         // This continues until i = 4, where arr[i] is arr[4], which gives you 19.
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+
+        //all indeces less than 5
+        // for (int i = 0; i < array.length; i++) {
+        //     System.out.println(array[i]);
+        // }
+
+
+        //Enhanced For Loop/For each Loop
+        //for every integer x that is present in this array, x is not the index but the value that is contained in this array
+        for(int x: array){
+            System.out.println(x);
         }
+
+
 
         int[] arr2 = new int[10];
 
