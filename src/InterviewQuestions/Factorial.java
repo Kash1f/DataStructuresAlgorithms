d96b385f34a6978bcd259e5cf15a96ed2926b3ff
Kashif <ahmed.kashif1001@gmail.com>
2024-10-02 22:13:23 +0500

    factorial in descending order

--- src/InterviewQuestions/Factorial.java
+++ src/InterviewQuestions/Factorial.java
@@ -3,7 +3,7 @@ package InterviewQuestions;
 public class Factorial {
     public static void main(String[] args) {
         
-
+        //Ascending Order
         int num = 5;
 
         long factorial = 1;
@@ -12,9 +12,18 @@ public class Factorial {
             factorial = factorial * i;
         }
 
-        System.out.println("Factorial is: " + factorial);
+        System.out.println("Factorial from method 1 is: " + factorial);
 
 
+        //Descending number
+        int num2 = 5;
+
+        long factorial2 = 1;
+
+        for(int i = num2; i >= 1; i--){
+            factorial2 = factorial2 * i;
+        }
+        System.out.println("Factorial from method 2 is: " + factorial2);
     }
 }
  
