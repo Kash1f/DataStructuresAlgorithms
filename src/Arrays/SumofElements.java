c17e7906146bf81f20331ad8c6889c7507443421
Kashif <ahmed.kashif1001@gmail.com>
2024-10-03 23:26:09 +0500

    sum of elements in array

--- src/Arrays/SumofElements.java
+++ src/Arrays/SumofElements.java
@@ -2,12 +2,12 @@ package Arrays;
 
 public class SumofElements {
     public static void main(String[] args) {
-        int array[] ={5, 2, 7, 8, 4};
+        int a[] ={5, 2, 7, 8, 4};
 
         int sum = 0;
 
-        for(int i = 0; i < array.length; i++){
-            sum = sum + array[i]; // 5+2+7+8+4
+        for(int i = 0; i < a.length; i++){
+            sum = sum + a[i]; // 5+2+7+8+4
         }
         System.out.println(sum);
     }
