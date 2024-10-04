eddba771085efd0544a072430659b80dcf215474
Kashif <ahmed.kashif1001@gmail.com>
2024-10-04 23:05:12 +0500

    check the equality of two arrays basic approach solution

---
+++ src/Arrays/ArrayEquality.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArrayEquality {
+    public static void main(String[] args) {
+        //Check the Equality of Two Arrays - Basic Approach
+
+        int array1[] = {1,2,3,4,5};
+        int array2[] = {1,2,3,4,5};
+
+        if(Arrays.equals(array1, array2)){
+            System.out.println("Arrays are Equal");
+            
+        } else {
+            System.out.println("Arrays are not Equal");
+        }
+
+    }
+}
