4c529617dc40855cf28e27187c35ee6a01737c9a
Kashif <ahmed.kashif1001@gmail.com>
2024-10-05 07:45:59 +0500

    array equality basic approach no 2

--- src/Arrays/ArrayEquality.java
+++ src/Arrays/ArrayEquality.java
@@ -6,15 +6,31 @@ public class ArrayEquality {
     public static void main(String[] args) {
         //Check the Equality of Two Arrays - Basic Approach
 
+        // int array1[] = {1,2,3,4,5};
+        // int array2[] = {1,2,3,4,5};
+
+        // if(Arrays.equals(array1, array2)){
+        //     System.out.println("Arrays are Equal");
+            
+        // } else {
+        //     System.out.println("Arrays are not Equal");
+        // }
+
+
+
+        //Check the Equality of Two Arrays - Basic Approach No 2
+
+
+        
         int array1[] = {1,2,3,4,5};
         int array2[] = {1,2,3,4,5};
 
-        if(Arrays.equals(array1, array2)){
-            System.out.println("Arrays are Equal");
-            
-        } else {
-            System.out.println("Arrays are not Equal");
-        }
+        boolean status = Arrays.equals(array1, array2);
 
+        if(status == true){
+            System.out.println("Arrays are Equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
     }
 }
