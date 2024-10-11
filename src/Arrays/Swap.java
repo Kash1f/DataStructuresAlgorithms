2ccefe5c5b2e96289f06cc7a560c1952eca89279
Kashif <ahmed.kashif1001@gmail.com>
2024-10-11 23:58:39 +0500

    logic no 2 to solve swap problem

--- src/Arrays/Swap.java
+++ src/Arrays/Swap.java
@@ -2,26 +2,25 @@ package Arrays;
 
 import java.util.Arrays;
 
-
-
-public class Swap{
+public class Swap {
     public static void main(String[] args) {
+
+        //Logic No 1 - Using Third Variable i.e temporary or temp variable
         int a = 10, b = 20;
-        System.out.println("Before Swapping Values are: " + a + "  " +b);
+        System.out.println("Before Swapping Values are: " + a + "  " + b);
 
-
+        //Logic No 1 - Using Third Variable i.e temporary or temp variable
         int temp = a;
         a = b;
         b = temp;
 
-        System.out.println("Before Swapping Values are: " + a + "  "  + b);
+        //Logic 2 - se + and -
 
+        a = a + b; // 30 
+        b = a - b; 
+        a = a - b;
+
+        System.out.println("Before Swapping Values are: " + a + "  " + b);
 
     }
 }
-
-
-
-
-
-
