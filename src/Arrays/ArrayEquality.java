fbf23c6803ea15403ab9b54111567ab7bfebee74
Kashif <ahmed.kashif1001@gmail.com>
2024-10-11 20:57:08 +0500

    factorial logic explanation

--- src/Arrays/ArrayEquality.java
+++ src/Arrays/ArrayEquality.java
@@ -4,33 +4,53 @@ import java.util.Arrays;
 
 public class ArrayEquality {
     public static void main(String[] args) {
-        //Check the Equality of Two Arrays - Basic Approach
+        // Check the Equality of Two Arrays - Basic Approach
 
         // int array1[] = {1,2,3,4,5};
         // int array2[] = {1,2,3,4,5};
 
         // if(Arrays.equals(array1, array2)){
-        //     System.out.println("Arrays are Equal");
-            
+        // System.out.println("Arrays are Equal");
+
         // } else {
-        //     System.out.println("Arrays are not Equal");
+        // System.out.println("Arrays are not Equal");
         // }
 
+        // Check the Equality of Two Arrays - Basic Approach No 2
+
+        // int array1[] = {1,2,3,4,5};
+        // int array2[] = {1,2,3,4,5};
+
+        // boolean status = Arrays.equals(array1, array2);
+
+        // if(status == true){
+        // System.out.println("Arrays are Equal");
+        // } else {
+        // System.out.println("Arrays are not equal");
+        // }
+
+        // Check the Equality of Two Arrays - Approach without Arrays.equal Method
+
+        int array1[] = { 1, 2, 3, 4, 5};
+        int array2[] = { 1, 2, 3, 4, 5 };
+
+        boolean status = true;
+
+        if(array1.length == array2.length){
+            for(int i = 0; i < array1.length; i++)
+            {
+                if(array1[i]!=array2[i])
+                {
+                    status = false;
+                }
+            }
+        } 
 
 
-        //Check the Equality of Two Arrays - Basic Approach No 2
 
 
         
-        int array1[] = {1,2,3,4,5};
-        int array2[] = {1,2,3,4,5};
 
-        boolean status = Arrays.equals(array1, array2);
-
-        if(status == true){
-            System.out.println("Arrays are Equal");
-        } else {
-            System.out.println("Arrays are not equal");
         }
+
     }
-}
--- src/InterviewQuestions/Factorial.java
+++ src/InterviewQuestions/Factorial.java
@@ -3,13 +3,16 @@ package InterviewQuestions;
 public class Factorial {
     public static void main(String[] args) {
         
-        //Ascending Order
+        //Ascending Order --> 5! = 1 * 2 * 3 * 4 * 5 = 120
         int num = 5;
 
         long factorial = 1;
 
+        //5 will be included in the calculation too so <= will be used here for num variable
         for(int i = 1; i <= num; i++){
-            factorial = factorial * i;
+
+            //
+            factorial = factorial * i; //1 //2 //6 //24 //120
         }
 
         System.out.println("Factorial from method 1 is: " + factorial);
@@ -26,4 +29,11 @@ public class Factorial {
         System.out.println("Factorial from method 2 is: " + factorial2);
     }
 }
- 
+
+
+//Logic behind this problem:
+
+// We have to multiply at each point, so in order to multiply 5 values from 1-5, we need to loop through them and multiply each value of loop to 
+// the factorial variable which is holding 1 initially but will change according to the condition when we will multiply it by value of i.
+
+// Humay har point pe multiply karna hai numbers ko, to agar 5 ka factorial nikalne k liye 1-5 multiplication karni hai to loop karna parega from 1-5, pehle factorial variable banadia hai jiski value 1 rakhdi hai taa k uski value loop k accordingly change ho kyun k hum factorial variable ki value ko loop wali values se multiply karwa rahe hain
