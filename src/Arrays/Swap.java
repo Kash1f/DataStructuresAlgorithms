539289fe225729837191427ded6600a83f0610fb
Kashif <ahmed.kashif1001@gmail.com>
2024-10-12 22:55:33 +0500

    logic 3 for swapping

--- src/Arrays/Swap.java
+++ src/Arrays/Swap.java
@@ -5,22 +5,30 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
 
-        //Logic No 1 - Using Third Variable i.e temporary or temp variable
+        //Logic 1 - Using Third Variable i.e temporary or temp variable
         int a = 10, b = 20;
         System.out.println("Before Swapping Values are: " + a + "  " + b);
 
         //Logic No 1 - Using Third Variable i.e temporary or temp variable
-        int temp = a;
-        a = b;
-        b = temp;
+        // int temp = a;
+        // a = b;
+        // b = temp;
 
-        //Logic 2 - se + and -
+        //Logic 2 use - se + and -
 
         a = a + b; // 30 
-        b = a - b; 
-        a = a - b;
+        b = a - b; // 30 - 20 = 10
+        a = a - b; //30 - 10 = 20
 
-        System.out.println("Before Swapping Values are: " + a + "  " + b);
+        //Logic 3 use * and /
+        //here a & b values should not be Zero
+
+        a = a * b;
+        b = a / b;
+        a = a / b;
+
+
+        System.out.println("After Swapping Values are: " + a + "  " + b);
 
     }
 }
