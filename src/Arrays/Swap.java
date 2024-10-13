887bbfc312a88b0bd1af48397862705c2becd79f
Kashif <ahmed.kashif1001@gmail.com>
2024-10-13 22:07:02 +0500

    swapping variables using XOR

--- src/Arrays/Swap.java
+++ src/Arrays/Swap.java
@@ -5,27 +5,35 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
 
-        //Logic 1 - Using Third Variable i.e temporary or temp variable
+        // Logic 1 - Using Third Variable i.e temporary or temp variable
         int a = 10, b = 20;
         System.out.println("Before Swapping Values are: " + a + "  " + b);
 
-        //Logic No 1 - Using Third Variable i.e temporary or temp variable
+        // Logic No 1 - Using Third Variable i.e temporary or temp variable
         // int temp = a;
         // a = b;
         // b = temp;
 
-        //Logic 2 use - se + and -
+        // Logic 2 use + and -
 
-        a = a + b; // 30 
-        b = a - b; // 30 - 20 = 10
-        a = a - b; //30 - 10 = 20
+        // a = a + b; // 30
+        // b = a - b; // 30 - 20 = 10
+        // a = a - b; // 30 - 10 = 20
 
-        //Logic 3 use * and /
-        //here a & b values should not be Zero
+        // Logic 3 use * and /
+        // here a & b values should not be Zero
 
-        a = a * b;
-        b = a / b;
-        a = a / b;
+        a = a * b; //10*20 = 200
+        b = a / b; //200/20 = 10
+        a = a / b; //200/10 = 20;
+
+
+        //Logic 4 - Bitwise XOR (^)
+
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
 
 
         System.out.println("After Swapping Values are: " + a + "  " + b);
