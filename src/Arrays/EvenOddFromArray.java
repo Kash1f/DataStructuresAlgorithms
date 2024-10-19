6c4053cc1b7dacbb99c30f6e0a1188bbb8a22c15
Kashif <ahmed.kashif1001@gmail.com>
2024-10-19 17:27:20 +0500

    even odd numbers in an array using enhanced for loop

--- src/Arrays/EvenOddFromArray.java
+++ src/Arrays/EvenOddFromArray.java
@@ -2,29 +2,33 @@ package Arrays;
 
 public class EvenOddFromArray {
     public static void main(String[] args) {
-        int a[] = {1, 2, 3, 4, 5, 6};
+        int a[] = { 1, 2, 3, 4, 5, 6 };
 
-        System.out.println("Even Numbers: ");
-        for(int i = 0; i < a.length; i++){
-            if(a[i] % 2 == 0){
-                System.out.println(a[i]);
-            }
+        // System.out.println("Even Numbers: ");
+        // for(int i = 0; i < a.length; i++){
+        // if(a[i] % 2 == 0){
+        // System.out.println(a[i]);
+        // }
+        // }
+
+        //This loop will retrieve Each value of array a[] and divide value by 2, here value contains Each element of the array
+        for (int value : a) {
+            if (value % 2 == 0)
+                System.out.println(value);
         }
 
-        System.out.println("Odd Numbers: ");
-        for(int i = 0; i < a.length; i++){
-            if(a[i]%2!=0){
-                System.out.println(a[i]);
-            }   
-        }
+        // System.out.println("Odd Numbers: ");
+        // for(int i = 0; i < a.length; i++){
+        // if(a[i]%2!=0){
+        // System.out.println(a[i]);
+        // }
+        // }
 
     }
 }
 
-
-
-
+// Approach 2 : Using Enhanced For Loop to print only even numbers
 
 // else if (a[i] % 2 != 0) {
-//     System.out.println(a[i]);
+// System.out.println(a[i]);
 // }
