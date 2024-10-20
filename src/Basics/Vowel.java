daa12a8ed1431916c9f28d98e5ece5fa89e1c818
Kashif <ahmed.kashif1001@gmail.com>
2024-10-20 21:44:36 +0500

    vowel basic problem

--- src/Basics/Vowel.java
+++ src/Basics/Vowel.java
@@ -1,12 +1,11 @@
 package Basics;
 
-
 public class Vowel {
     public static void main(String[] args) {
-       
+
         char v = 'e';
 
-        if(v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u'){
+        if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u') {
             System.out.println("Vowel");
         } else {
             System.out.println("Not Vowel");
