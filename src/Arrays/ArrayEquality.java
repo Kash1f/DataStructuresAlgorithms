df04cd5391319d15928aa5ada44b73e83c0543c0
Kashif <ahmed.kashif1001@gmail.com>
2024-10-20 23:33:50 +0500

    positive negative zero problem

--- src/Arrays/ArrayEquality.java
+++ src/Arrays/ArrayEquality.java
@@ -18,38 +18,44 @@ public class ArrayEquality {
 
         // Check the Equality of Two Arrays - Basic Approach No 2
 
-        // int array1[] = {1,2,3,4,5};
-        // int array2[] = {1,2,3,4,5};
+        int array1[] = {1,2,3,4,5};
+        int array2[] = {1,2,3,4,5};
 
-        // boolean status = Arrays.equals(array1, array2);
+        boolean status = Arrays.equals(array1, array2);
 
-        // if(status == true){
-        // System.out.println("Arrays are Equal");
-        // } else {
-        // System.out.println("Arrays are not equal");
-        // }
+        if(status == true){
+        System.out.println("Arrays are Equal");
+        } else {
+        System.out.println("Arrays are not equal");
+        }
 
         // Check the Equality of Two Arrays - Approach without Arrays.equal Method
 
-        int array1[] = { 1, 2, 3, 4, 5};
-        int array2[] = { 1, 2, 3, 4, 5 };
+        int arr1[] = { 1, 2, 3, 4, 5 };
+        int arr2[] = { 1, 2, 3, 4, 5 };
 
-        boolean status = true;
+        boolean status2 = true;
 
-        if(array1.length == array2.length){
-            for(int i = 0; i < array1.length; i++)
-            {
-                if(array1[i]!=array2[i])
-                {
-                    status = false;
+        // Check if the lengths of the arrays are equal
+        if(arr1.length == arr2.length) {
+            // Compare each element of both arrays
+            for(int i = 0; i < arr1.length; i++) {
+                if(arr1[i] != arr2[i]) {
+                    status2 = false; // If any element differs, set status to false
+                    break; // No need to check further if a mismatch is found
                 }
             }
-        } 
+        } else {
+            status2 = false; // If lengths are not equal, they are not the same
+        }
 
+        // Print the result
+        if(status2) {
+            System.out.println("Both arrays are equal.");
+        } else {
+            System.out.println("Arrays are not equal.");
+        }
 
-
-
-        
 
         }
 
--- src/Basics/PrimeNumber.java
+++ src/Basics/PrimeNumber.java
@@ -1,21 +1,25 @@
 package Basics;
 
-public class PrimeNumber {
-    public static void main(String[] args) {
+public class PrimeNumber { // This defines a public class named PrimeNumber
+    public static void main(String[] args) { // The main method is the entry point of the program
 
-        int num = 7;
-        int count = 0;
+        int num = 7; // Declare an integer variable 'num' and initialize it with the value 7
+        int count = 0; // Declare another integer variable 'count' to keep track of how many divisors
+                       // 'num' has
 
+        // Loop through all numbers from 1 to 'num'
         for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                count++;
+            // Check if 'num' is divisible by 'i'
+            if (num % i == 0) { // The % operator gives the remainder of the division
+                count++; // If 'num' is divisible by 'i', increment the count by 1
             }
         }
-        if (count == 2) {
-            System.out.println("Prime");
+
+        // After the loop, check how many divisors 'num' has
+        if (count == 2) { // A prime number has exactly 2 divisors: 1 and itself
+            System.out.println("Prime"); // If the count is 2, print "Prime"
         } else {
-            System.out.println("Not Prime");
+            System.out.println("Not Prime"); // Otherwise, print "Not Prime"
         }
     }
-
 }
